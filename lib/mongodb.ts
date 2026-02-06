0184953727e649360c6af02d9fc067bf3f478fb1
Marcus-devfs <marcusvini6277@gmail.com>
2026-02-06 16:42:33 -0300

    fix mongodb

--- lib/mongodb.ts
+++ lib/mongodb.ts
@@ -2,9 +2,7 @@ import mongoose from 'mongoose';
 
 const MONGODB_URI = process.env.MONGODB_URI || process.env.MONGO_URI;
 
-if (!MONGODB_URI) {
-    throw new Error('Please define the MONGODB_URI environment variable inside .env.local');
-}
+
 
 interface MongooseCache {
     conn: typeof mongoose | null;
